f2434ac0be41d92fa33ac07113d387cf508afbe4
Miguel Vieira Menezes da Silva <gitprogama@gmail.com>
2025-05-03 16:56:43 -0300

    coloquei aspas

--- projeto-fake-pinterest/main.py
+++ projeto-fake-pinterest/main.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def homepage():
     return "Fake Pinterest - Meu primeiro projeto"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
