ba6d799f8ef0cb865d3b7c7d68e13398ec498c9f
Miguel Vieira Menezes da Silva <gitprogama@gmail.com>
2025-05-09 13:47:33 -0300

    database.column corrected.

--- FakePinterest/models.py
+++ FakePinterest/models.py
@@ -4,14 +4,14 @@ from datetime import datetime
 
 
 class User(database.Model):
-    id = database.Collumn(database.Integer, primary_key=True)
-    username = database.Collumn(database.String, nullable=False)
-    email = database.Collumn(database.String, nullable=False, unique=True)
-    senha = database.Collumn(database.String, nullable=False)
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String, nullable=False)
+    email = database.Column(database.String, nullable=False, unique=True)
+    senha = database.Column(database.String, nullable=False)
     post = database.Relationship("Post", backref="User", lazy=True)
 
 class Post(database.Model):
-    id = database.Collumn(database.Integer, primary_key=True)
-    imagem = database.Collumn(database.String, default="default.png")
-    data_criacao = database.Collumn(database.DateTime, nullable=False,     default=datetime.utcnow())
-    id_usuario = database.Collumn(database.Integer, database.ForeignKey("user.id"), nullable=False)
+    id = database.Column(database.Integer, primary_key=True)
+    imagem = database.Column(database.String, default="default.png")
+    data_criacao = database.Column(database.DateTime, nullable=False,     default=datetime.utcnow())
+    id_usuario = database.Column(database.Integer, database.ForeignKey("user.id"), nullable=False)
