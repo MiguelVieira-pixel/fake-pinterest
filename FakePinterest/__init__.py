2fcc11cccdfca12e7f9d29060a6baed82c6072d4
Miguel Vieira Menezes da Silva <gitprogama@gmail.com>
2025-05-09 14:26:58 -0300

    add login system and security

--- FakePinterest/__init__.py
+++ FakePinterest/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SECRET_KEY"] = "8aacf9bc311f74cc8d5383ca525bc883"
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
 
 from FakePinterest import routes
--- FakePinterest/routes.py
+++ FakePinterest/routes.py
@@ -1,11 +1,12 @@
 #criar as rotas do nosso site(links)
 from flask import render_template, url_for
 from FakePinterest import app
-
+from flask_login import login_required
 @app.route("/")
 def homepage():
     return render_template("index.html")
 
 @app.route("/profile/<user>")
+@login_required
 def profile(user):
     return render_template("profile.html", user=user)
