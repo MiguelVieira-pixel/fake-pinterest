742e87cb6a3c6a7b3ee5bfc25657c6769c013dd7
Miguel Vieira Menezes da Silva <gitprogama@gmail.com>
2025-05-10 17:14:14 -0300

    criated login form.

--- FakePinterest/forms.py
+++ FakePinterest/forms.py
@@ -1,1 +1,23 @@
 #criar os formularios do site
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from FakePinterest.models import User
+
+class FormLogin(FlaskForm):
+    imail = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+class FormCreateCount(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    username = StringField("Username", validators=[DataRequired()])
+    passoword = PasswordField("Passoword", validators=[DataRequired(), Length(6,20), EqualTo("Confirm")])
+    confirm = PasswordField("Confirm", validators=[DataRequired(), EqualTo("Passoword")])
+    submit = SubmitField("Create Account")
+
+def validate_email(self, email):
+    user = User.query.filter_by(email=email.data).first()
+    if user:
+        return ValidationError("Email already registered")
+    
