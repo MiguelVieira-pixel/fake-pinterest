21535e13eb345564f1904003c3d48a26468821a6
Miguel Vieira Menezes da Silva <gitprogama@gmail.com>
2025-05-20 13:51:01 -0300

    add image management .

--- FakePinterest/__init__.py
+++ FakePinterest/__init__.py
@@ -6,6 +6,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 app.config["SECRET_KEY"] = "8aacf9bc311f74cc8d5383ca525bc883"
+app.config["UPLOAD_FOLDER"] = "static/photos_posts"
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
